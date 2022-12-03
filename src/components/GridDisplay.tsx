34e4d40053bddfb167e3a11216d821b274e570a4
crespire <li.simmon@gmail.com>
2022-12-02 23:19:32 -0500

    feat: mobile-first column setup

--- src/components/GridDisplay.tsx
+++ src/components/GridDisplay.tsx
@@ -3,7 +3,7 @@ import ThreadPreview from "./ThreadPreview";
 
 function GridDisplay({threads}: {threads: ThreadModel[]}) {
   return (
-    <main className="grid grid-cols-2 gap-1">
+    <main className="grid grid-cols-2 gap-1 md:grid-cols-3 md:gap-2 lg:grid-cols-4 lg:gap-3">
       { threads.map((thread, index) => {
         return <ThreadPreview key={index} thread={thread} />
       })}
