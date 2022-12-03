730597e8997cc1a5bd1aea2c425fec3648b554b9
crespire <li.simmon@gmail.com>
2022-12-02 22:51:04 -0500

    feat: add tooltip on hover on thread preview

--- src/components/ThreadPreview.tsx
+++ src/components/ThreadPreview.tsx
@@ -1,10 +1,26 @@
 import { ThreadModel } from "../@types/thread";
+import { useRef } from 'react';
+import DOMPurify from 'dompurify';
 
 function ThreadPreview({thread}: {thread: ThreadModel}) {
+  const postCount = thread.posts.length;
+  const spanRef = useRef<HTMLElement>(null);
+  const showTooltip = () => {
+    spanRef?.current?.setAttribute('title', `This thread has ${postCount} posts.`);
+  };
+  const removeTooltip = () => {
+    spanRef?.current?.removeAttribute('title');
+  }
+  const cleanCom = (com:string) => {
+    return {__html: DOMPurify.sanitize(com)}
+  }
+
   return (
-    <article>
-      <header>{ thread.posts[0].semantic_url}</header>
-      <main>{ thread.posts[0].com }</main>
+    <article className="p-2">
+      <header>
+        <span ref={spanRef} className="text-2xl" onMouseOver={showTooltip} onMouseLeave={removeTooltip}>{ thread.posts[0].semantic_url}</span>
+      </header>
+      <main dangerouslySetInnerHTML={cleanCom(thread.posts[0].com)}></main>
     </article>
   );
 }
