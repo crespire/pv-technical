9362c84b3ec6c84106be3b552f5562d3577476c3
crespire <li.simmon@gmail.com>
2022-12-04 18:00:47 -0500

    style: fix responsive styles for grid

--- src/components/GridDisplay.tsx
+++ src/components/GridDisplay.tsx
@@ -3,7 +3,10 @@ import ThreadPreview from "./ThreadPreview";
 
 function GridDisplay({threads}: {threads: ThreadModel[]}) {
   return (
-    <div className="grid grid-cols-2 gap-2 md:grid-cols-3 md:gap-3 lg:grid-cols-4 lg:gap-4">
+    <div
+      className="grid grid-cols-2 gap-2 md:grid-cols-3 md:gap-3 \
+                 lg:grid-cols-4 lg:gap-4"
+    >
       { threads.map((thread, index) => {
         return <ThreadPreview key={index} thread={thread} />
       })}
