d3a0c86f4b7bb496b004d200da4b27967848b0f5
crespire <li.simmon@gmail.com>
2022-12-04 14:33:59 -0500

    style: add semicolons

--- src/components/ThreadPreview.tsx
+++ src/components/ThreadPreview.tsx
@@ -10,10 +10,10 @@ function ThreadPreview({thread}: {thread: ThreadModel}) {
   };
   const removeTooltip = () => {
     spanRef?.current?.removeAttribute('title');
-  }
+  };
   const cleanCom = (com:string) => {
     return {__html: DOMPurify.sanitize(com)}
-  }
+  };
 
   return (
     <article className="p-2 bg-inherit border border-solid border-slate-200 drop-shadow-none max-w-prose h-36 max-h-36 truncate transition ease-in-out duration-100 hover:-translate-y-1 hover:translate-x-1 hover:shadow-lg hover:z-100">
