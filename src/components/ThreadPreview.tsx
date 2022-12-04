d39101248953032d3bcddc303be79288a033a4dd
crespire <li.simmon@gmail.com>
2022-12-04 15:27:17 -0500

    fix: move ref to article

--- src/components/ThreadPreview.tsx
+++ src/components/ThreadPreview.tsx
@@ -4,21 +4,26 @@ import DOMPurify from 'dompurify';
 
 function ThreadPreview({thread}: {thread: ThreadModel}) {
   const postCount = thread.posts.length;
-  const spanRef = useRef<HTMLElement>(null);
+  const articleRef = useRef<HTMLElement>(null);
   const showTooltip = () => {
-    spanRef?.current?.setAttribute('title', `This thread has ${postCount} posts.`);
+    articleRef?.current?.setAttribute('title', `This thread has ${postCount} posts.`);
   };
   const removeTooltip = () => {
-    spanRef?.current?.removeAttribute('title');
+    articleRef?.current?.removeAttribute('title');
   };
   const cleanCom = (com:string) => {
-    return {__html: DOMPurify.sanitize(com)}
+    return {__html: DOMPurify.sanitize(com)};
   };
 
   return (
-    <article className="p-2 bg-inherit border border-solid border-slate-200 drop-shadow-none max-w-prose h-36 max-h-36 truncate transition ease-in-out duration-100 hover:-translate-y-1 hover:translate-x-1 hover:shadow-lg hover:z-100">
+    <article
+      ref={articleRef}
+      onMouseOver={showTooltip}
+      onMouseLeave={removeTooltip}
+      className="p-2 bg-inherit border border-solid border-slate-200 drop-shadow-none max-w-prose h-36 max-h-36 truncate transition ease-in-out duration-100 hover:-translate-y-1 hover:translate-x-1 hover:shadow-lg hover:z-100"
+    >
       <header className="truncate">
-        <span ref={spanRef} className="text-2xl" onMouseOver={showTooltip} onMouseLeave={removeTooltip}>{ thread.posts[0].semantic_url}</span>
+        <span className="text-2xl">{thread.posts[0].semantic_url}</span>
       </header>
       <main dangerouslySetInnerHTML={cleanCom(thread.posts[0].com)} className="truncate"></main>
     </article>
