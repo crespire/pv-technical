cf783bce0aff701f14e15227167a1d1cffb80514
crespire <li.simmon@gmail.com>
2022-12-04 20:07:05 -0500

    feat: use CORS proxy service to retrieve data

--- src/components/ThreadsIndex.tsx
+++ src/components/ThreadsIndex.tsx
@@ -11,14 +11,34 @@ function ThreadIndex() {
   useEffect(() => {
     const getThreads = async () => {
       let data = null;
+      let local = false;
       try {
+        /**
+         * Trying to access https://a.4cdn.org/po/2.json fails with error:
+         * 
+         * Cross-Origin Request Blocked: The Same Origin Policy disallows
+         * reading the remote resource at https://a.4cdn.org/po/2.json.
+         * (Reason: CORS header “Access-Control-Allow-Origin” does not
+         * match “http://boards.4chan.org”).
+         * 
+         * I have worked around the CORS issue using a CORS proxy service. I
+         * cannot guarantee this service stays up. In my testing, it allows the
+         * client application to fetch the JSON from the server.
+         * 
+         * If this frontend were fetching from a backend I controlled, the
+         * solution would be to configure the CORS Headers on the server side
+         * to allow the frontend domain to make cross origin requests.
+         */
+        data = await fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('https://a.4cdn.org/po/2.json')}`)
+                      .then(response => response.json()); 
+      } catch (err) {
+        console.log('Got error: ', err);
+        console.log('Loading local data from assets...');
         data = await import('../assets/data.json')
                       .then(response => response.threads);
-      } catch (err) {
-        setThreads([]);
-        console.log('Got error: ', err);
+        local = true;
       }
-      setThreads(data);
+      local ? setThreads(data) : setThreads(JSON.parse(data.contents).threads);
     };
 
     getThreads();
